6a00e41f4d6c306ac6109cdca91f82a013124ebb
medha.pundir <medha.pundir@gmail.com>
2018-01-23 16:05:18 +0530

    updated

--- src/main/java/com/iris/spring/tasklet/App.java
+++ src/main/java/com/iris/spring/tasklet/App.java
@@ -1,5 +1,12 @@
 package com.iris.spring.tasklet;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -9,5 +16,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        ApplicationContext ctx=new ClassPathXmlApplicationContext("job.xml");
+        Job job=(Job) ctx.getBean("springBatchCsvToDbJob");
+        JobLauncher launcher=(JobLauncher)ctx.getBean("jobLauncher");
+        try
+        {
+        	JobExecution execution=launcher.run(job,new JobParameters());
+        	System.out.println(execution.getExitStatus());
+        	
+        }catch(Exception i)
+        {
+        	i.printStackTrace();
+        }
+        
     }
 }
